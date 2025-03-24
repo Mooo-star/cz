6be103678e8e2c8cc7e8e608401750c65eb8d646
sunny <18833104957@163.com>
2025-03-24 19:25:21 +0800

    test(ceshi): 测试

--- packages/cz/src/commands/index.ts
+++ packages/cz/src/commands/index.ts
@@ -1,10 +1,8 @@
 import { createCommand } from "./create";
-import { infoCommand } from "./info";
 import { Command } from "commander";
 
 // 简化后的 registerCommands 函数
 export const registerCommands = async (program: Command) => {
   // 注册所有命令
   createCommand(program);
-  infoCommand(program);
 };
